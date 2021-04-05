35eef7f2a57eebe2b4b5f00c3ac50b317ae7db86
gabriel.dahan <gabriel.dahan@monlycee.net>
2021-04-05 14:07:02 +0200

    First Release

---
+++ example.py
@@ -0,0 +1,6 @@
+from encrypt import Key
+
+if __name__ == '__main__':
+    # Be carefull, you can definitely lose your file if you're trying to decrypt it with the wrong key.
+    k = Key("your-key")
+    k.fcrypt('example-files/file.txt', out = 'example-files/file.txt.crypt')
